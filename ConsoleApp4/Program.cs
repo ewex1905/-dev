3f2aecace13af292456a54687d19df3cd1803bd9
erener <erener@DESKTOP-KPUBUN9>
2023-06-02 23:15:19 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isim = { "H", "Ü", "S", "E","Y","İ","N" };
+            Console.WriteLine(isim[0]); 
+            for (int i = 1; i < isim.Length; i++)
+            {
+                Console.WriteLine(isim[i]);
+            }
+        }
+    }
+}
